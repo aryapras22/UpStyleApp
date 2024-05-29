eaf335f14062ccaced3b2160e20e93aa4b92f050
aryapras22 <88756038+aryapras22@users.noreply.github.com>
2024-05-29 22:30:13 +0700

    ui-2

--- lib/screen/home/home.dart
+++ lib/screen/home/home.dart
@@ -86,165 +86,184 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.surface,
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Stack(
-              children: [
-                Container(
-                  height: 330,
-                  decoration: BoxDecoration(
-                    // bottom left and right radius
-                    borderRadius: BorderRadius.only(
-                      bottomLeft: Radius.circular(20),
-                      bottomRight: Radius.circular(20),
-                    ),
-                    image: DecorationImage(
-                        image: Image.asset(
-                          'assets/images/home_cover.png',
-                        ).image,
-                        fit: BoxFit.fill),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 150, left: 20, right: 20),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        'Lorem Ipsum',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 28,
-                          fontWeight: FontWeight.bold,
-                        ),
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            expandedHeight: 310,
+            floating: false,
+            pinned: true,
+            flexibleSpace: FlexibleSpaceBar(
+              background: Stack(
+                fit: StackFit.expand,
+                children: [
+                  Container(
+                    height: 330,
+                    decoration: BoxDecoration(
+                      // bottom left and right radius
+                      borderRadius: BorderRadius.only(
+                        bottomLeft: Radius.circular(20),
+                        bottomRight: Radius.circular(20),
                       ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 40.0),
-                        child: Center(
-                          child: Text(
-                            'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
-                            textAlign: TextAlign.center,
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 16,
-                              fontWeight: FontWeight.bold,
+                      image: DecorationImage(
+                          image: Image.asset(
+                            'assets/images/home_cover.png',
+                          ).image,
+                          fit: BoxFit.fill),
+                    ),
+                  ),
+                  Padding(
+                    padding:
+                        const EdgeInsets.only(top: 150, left: 20, right: 20),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'Lorem Ipsum',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 28,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 40.0),
+                          child: Center(
+                            child: Text(
+                              'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                              ),
                             ),
                           ),
                         ),
-                      ),
-                      SizedBox(height: 20),
-                      ElevatedButton.icon(
-                        onPressed: () {
-                          _showUploadDialog(context);
-                        },
-                        icon: Image.asset('assets/icons/upload.png'),
-                        label: Text('Upload your clothes',
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontWeight: FontWeight.w400,
-                              // use theme data font
-                            )),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Expanded(
-                    child: TextField(
-                      decoration: InputDecoration(
-                        focusColor: Colors.white,
-                        fillColor: Colors.white,
-                        filled: true,
-                        hintText: 'Lorem Ipsum is simply',
-                        prefixIcon: Icon(
-                          Icons.search,
-                          color: Theme.of(context).colorScheme.tertiary,
-                          size: 25,
+                        SizedBox(height: 20),
+                        ElevatedButton.icon(
+                          onPressed: () {
+                            _showUploadDialog(context);
+                          },
+                          icon: Image.asset('assets/icons/upload.png'),
+                          label: Text('Upload your clothes',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.w400,
+                                // use theme data font
+                              )),
                         ),
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10),
-                          borderSide: BorderSide.none,
-                        ),
-                      ),
-                    ),
-                  ),
-                  SizedBox(width: 10),
-                  Container(
-                    height: 50,
-                    width: 50,
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    child: Icon(
-                      Icons.filter_list,
-                      color: Theme.of(context).colorScheme.primary,
+                      ],
                     ),
                   ),
                 ],
               ),
             ),
-            // recommended for you row
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text('Recommended for you',
-                      style: Theme.of(context).textTheme.titleLarge),
-                  // icon next
-                  Icon(
-                    Icons.arrow_forward_ios,
-                    color: Colors.black,
-                    size: 16,
-                  ),
-                ],
-              ),
-            ),
-            SizedBox(height: 10),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Chip(
-                    label: Text('Trend'),
-                    backgroundColor: Theme.of(context).colorScheme.primary,
-                    labelStyle: TextStyle(color: Colors.white),
-                    // remove outline
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                  ),
-                  Chip(
-                    label: Text('Nearby'),
-                    backgroundColor: Theme.of(context).colorScheme.onSecondary,
-                  ),
-                  Chip(label: Text('Nearby')),
-                  Chip(label: Text('Nearby')),
-                ],
-              ),
-            ),
-            SizedBox(height: 20),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          ),
+          SliverPersistentHeader(
+            pinned: true,
+            delegate: SliverPersistentHeaderDelegate(
               child: Column(
                 children: [
-                  PostCard(),
-                  PostCard(),
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Expanded(
+                          child: TextField(
+                            decoration: InputDecoration(
+                              focusColor: Colors.white,
+                              fillColor: Colors.white,
+                              filled: true,
+                              hintText: 'Lorem Ipsum is simply',
+                              prefixIcon: Icon(
+                                Icons.search,
+                                color: Theme.of(context).colorScheme.tertiary,
+                                size: 25,
+                              ),
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10),
+                                borderSide: BorderSide.none,
+                              ),
+                            ),
+                          ),
+                        ),
+                        SizedBox(width: 10),
+                        Container(
+                          height: 50,
+                          width: 50,
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: Icon(
+                            Icons.filter_list,
+                            color: Theme.of(context).colorScheme.primary,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  // recommended for you row
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text('Recommended for you',
+                            style: Theme.of(context).textTheme.titleLarge),
+                        // icon next
+                        Icon(
+                          Icons.arrow_forward_ios,
+                          color: Colors.black,
+                          size: 16,
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Chip(
+                          label: Text('Trend'),
+                          backgroundColor:
+                              Theme.of(context).colorScheme.primary,
+                          labelStyle: TextStyle(color: Colors.white),
+                          // remove outline
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                        Chip(
+                          label: Text('Nearby'),
+                          backgroundColor:
+                              Theme.of(context).colorScheme.onSecondary,
+                        ),
+                        Chip(label: Text('Nearby')),
+                        Chip(label: Text('Nearby')),
+                      ],
+                    ),
+                  ),
                 ],
               ),
             ),
-          ],
-        ),
+            
+          ),
+          SliverList(
+            delegate: SliverChildBuilderDelegate(
+              (BuildContext context, int index) {
+                return Container(
+                    margin: EdgeInsets.symmetric(horizontal: 16),
+                    child: PostCard());
+              },
+              childCount: 10,
+            ),
+          ),
+        ],
       ),
     );
   }
