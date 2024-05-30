05d36e194071561ef5d28963def72daf8edb5364
aryapras22 <88756038+aryapras22@users.noreply.github.com>
2024-05-30 13:53:59 +0700

    loading

--- lib/screen/home/home.dart
+++ lib/screen/home/home.dart
@@ -1,12 +1,11 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, unused_local_variable
 
 import 'dart:io';
-import 'dart:math';
-
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:dotted_border/dotted_border.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:pull_to_refresh_flutter3/pull_to_refresh_flutter3.dart';
 import 'package:upstyleapp/model/post.dart';
 import 'package:upstyleapp/services/post_service.dart';
 import 'package:upstyleapp/widgets/post_card.dart';
@@ -26,6 +25,14 @@ class _HomeState extends State<Home> {
   bool isLoading = true;
   final PostService _postService = PostService();
   List<Post> posts = [];
+
+  final RefreshController _refreshController =
+      RefreshController(initialRefresh: false);
+
+  void _onRefresh() async {
+    await _fetchData();
+    _refreshController.refreshCompleted();
+  }
 
   void _showUploadDialog(BuildContext context) {
     showDialog(
@@ -160,7 +167,7 @@ class _HomeState extends State<Home> {
                           content: Column(
                             mainAxisSize: MainAxisSize.min,
                             children: [
-                              Image.asset('assets/icons/upload_success.png'),
+                              Image.asset('assets/images/upload_success.png'),
                               SizedBox(height: 10),
                               Text(
                                 'Your image has been uploaded successfully.',
@@ -191,11 +198,13 @@ class _HomeState extends State<Home> {
     try {
       await _postService.createPost(_captionController.text, _image!);
     } catch (e) {
-      print(e);
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+        content: Text('An error occurred. Please try again.'),
+      ));
     }
   }
 
-  void _fetchData() async {
+  Future<void> _fetchData() async {
     var allPost = await _postService.readAllPosts();
     String name = '';
     String avatar = '';
@@ -229,169 +238,198 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.surface,
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Stack(
-              children: [
-                Container(
-                  height: 330,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.only(
-                      bottomLeft: Radius.circular(20),
-                      bottomRight: Radius.circular(20),
-                    ),
-                    image: DecorationImage(
-                        image: Image.asset(
-                          'assets/images/home_cover.png',
-                        ).image,
-                        fit: BoxFit.fill),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 150, left: 20, right: 20),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        'Lorem Ipsum',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 28,
-                          fontWeight: FontWeight.bold,
-                        ),
+      body: SmartRefresher(
+        enablePullDown: true,
+        enablePullUp: false,
+        header: WaterDropHeader(),
+        footer: CustomFooter(
+          builder: (BuildContext context, LoadStatus? mode) {
+            Widget body;
+            if (mode == LoadStatus.idle) {
+              body = Text("pull up load");
+            } else if (mode == LoadStatus.loading) {
+              body = CupertinoActivityIndicator();
+            } else if (mode == LoadStatus.failed) {
+              body = Text("Load Failed!Click retry!");
+            } else if (mode == LoadStatus.canLoading) {
+              body = Text("release to load more");
+            } else {
+              body = Text("No more Data");
+            }
+            return Container(
+              height: 55.0,
+              child: Center(child: body),
+            );
+          },
+        ),
+        controller: _refreshController,
+        onRefresh: _onRefresh,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Stack(
+                children: [
+                  Container(
+                    height: 330,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.only(
+                        bottomLeft: Radius.circular(20),
+                        bottomRight: Radius.circular(20),
                       ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 40.0),
-                        child: Center(
-                          child: Text(
-                            'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
-                            textAlign: TextAlign.center,
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 16,
-                              fontWeight: FontWeight.bold,
+                      image: DecorationImage(
+                          image: Image.asset(
+                            'assets/images/home_cover.png',
+                          ).image,
+                          fit: BoxFit.fill),
+                    ),
+                  ),
+                  Padding(
+                    padding:
+                        const EdgeInsets.only(top: 150, left: 20, right: 20),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'Lorem Ipsum',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 28,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 40.0),
+                          child: Center(
+                            child: Text(
+                              'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                              ),
                             ),
                           ),
                         ),
-                      ),
-                      SizedBox(height: 20),
-                      ElevatedButton.icon(
-                        onPressed: () {
-                          _showUploadDialog(context);
-                        },
-                        icon: Image.asset('assets/icons/upload.png'),
-                        label: Text('Upload your clothes',
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontWeight: FontWeight.w400,
-                              // use theme data font
-                            )),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Expanded(
-                    child: TextField(
-                      decoration: InputDecoration(
-                        focusColor: Colors.white,
-                        fillColor: Colors.white,
-                        filled: true,
-                        hintText: 'Lorem Ipsum is simply',
-                        prefixIcon: Icon(
-                          Icons.search,
-                          color: Theme.of(context).colorScheme.tertiary,
-                          size: 25,
+                        SizedBox(height: 20),
+                        ElevatedButton.icon(
+                          onPressed: () {
+                            _showUploadDialog(context);
+                          },
+                          icon: Image.asset('assets/icons/upload.png'),
+                          label: Text('Upload your clothes',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.w400,
+                                // use theme data font
+                              )),
                         ),
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10),
-                          borderSide: BorderSide.none,
-                        ),
-                      ),
-                    ),
-                  ),
-                  SizedBox(width: 10),
-                  Container(
-                    height: 50,
-                    width: 50,
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    child: Icon(
-                      Icons.filter_list,
-                      color: Theme.of(context).colorScheme.primary,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            // recommended for you row
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text('Recommended for you',
-                      style: Theme.of(context).textTheme.titleLarge),
-                  // icon next
-                  Icon(
-                    Icons.arrow_forward_ios,
-                    color: Colors.black,
-                    size: 16,
-                  ),
-                ],
-              ),
-            ),
-            SizedBox(height: 10),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Chip(
-                    label: Text('Trend'),
-                    backgroundColor: Theme.of(context).colorScheme.primary,
-                    labelStyle: TextStyle(color: Colors.white),
-                    // remove outline
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                  ),
-                  Chip(
-                    label: Text('Nearby'),
-                    backgroundColor: Theme.of(context).colorScheme.onSecondary,
-                  ),
-                  Chip(label: Text('Nearby')),
-                  Chip(label: Text('Nearby')),
-                ],
-              ),
-            ),
-            SizedBox(height: 20),
-            isLoading
-                ? CircularProgressIndicator(
-                    color: Theme.of(context).colorScheme.primary,
-                  )
-                : Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                    child: Column(
-                      children: [
-                        for (var post in posts)
-                          PostCard(
-                            post: post,
-                          ),
                       ],
                     ),
                   ),
-          ],
+                ],
+              ),
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Expanded(
+                      child: TextField(
+                        decoration: InputDecoration(
+                          focusColor: Colors.white,
+                          fillColor: Colors.white,
+                          filled: true,
+                          hintText: 'Lorem Ipsum is simply',
+                          prefixIcon: Icon(
+                            Icons.search,
+                            color: Theme.of(context).colorScheme.tertiary,
+                            size: 25,
+                          ),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                            borderSide: BorderSide.none,
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(width: 10),
+                    Container(
+                      height: 50,
+                      width: 50,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Icon(
+                        Icons.filter_list,
+                        color: Theme.of(context).colorScheme.primary,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              // recommended for you row
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text('Recommended for you',
+                        style: Theme.of(context).textTheme.titleLarge),
+                    // icon next
+                    Icon(
+                      Icons.arrow_forward_ios,
+                      color: Colors.black,
+                      size: 16,
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 10),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Chip(
+                      label: Text('Trend'),
+                      backgroundColor: Theme.of(context).colorScheme.primary,
+                      labelStyle: TextStyle(color: Colors.white),
+                      // remove outline
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    Chip(
+                      label: Text('Nearby'),
+                      backgroundColor:
+                          Theme.of(context).colorScheme.onSecondary,
+                    ),
+                    Chip(label: Text('Nearby')),
+                    Chip(label: Text('Nearby')),
+                  ],
+                ),
+              ),
+              SizedBox(height: 20),
+              isLoading
+                  ? CircularProgressIndicator(
+                      color: Theme.of(context).colorScheme.primary,
+                    )
+                  : Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                      child: Column(
+                        children: [
+                          for (var post in posts)
+                            PostCard(
+                              post: post,
+                            ),
+                        ],
+                      ),
+                    ),
+            ],
+          ),
         ),
       ),
     );
