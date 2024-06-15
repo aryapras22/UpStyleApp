755a09069ed4c46e11270221caa2784575170121
Muhammad Yusuf <muhammadyusuf@Muhammads-MacBook-Air-4.local>
2024-06-15 22:26:25 +0700

    order finished

---
+++ lib/model/post.dart
@@ -0,0 +1,18 @@
+class Post {
+  String id;
+  String name;
+  String userAvatar;
+  String postImage;
+  String caption;
+  String time;
+  String userId;
+
+  Post(
+      {required this.id,
+      required this.name,
+      required this.userAvatar,
+      required this.postImage,
+      required this.caption,
+      required this.time,
+      required this.userId});
+}
